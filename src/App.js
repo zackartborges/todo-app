8c1e5d94c6ed93e2b564fbab25a4ddf7f09d6ad3
Zack Borges <zackartborges@gmail.com>
2021-09-21 16:01:32 -0400

    loads imported todosData into the state of the component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import './App.css';
 import { Component } from 'react';
 import ToDoItem from './components/ToDoItem'
-import todosData from './todosData';
+// import todosData from './todosData';
 
 class App extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      todosData:
+        [
+          {
+              id: 1,
+              text: "Take out the trash",
+              completed: true
+          },
+          {
+              id: 2,
+              text: "Grocery shopping",
+              completed: false
+          },
+          {
+              id: 3,
+              text: "Clean gecko tank",
+              completed: false
+          },
+          {
+              id: 4,
+              text: "Mow lawn",
+              completed: true
+          },
+          {
+              id: 5,
+              text: "Catch up on Arrested Development",
+              completed: false
+          }
+        ]
+      
+    }
+  }
   render() {
-        const todoTitles = todosData.map(todoItem => 
+        const todoTitles = this.state.todosData.map(todoItem => 
            <ToDoItem key={todoItem.id} task={todoItem.text} />
         )
         return (
