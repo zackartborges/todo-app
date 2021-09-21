09d24266a07cdf09ea3013aa077fb7a144babcab
Zack Borges <zackartborges@gmail.com>
2021-09-21 15:58:07 -0400

    changes functional based component to a class based component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
+import { Component } from 'react';
 import ToDoItem from './components/ToDoItem'
 import todosData from './todosData';
 
-function App() {
-    const todoTitles = todosData.map(todoItem => 
-       <ToDoItem key={todoItem.id} task={todoItem.text} />
-    )
-
-  return (
-    <div className="todo-list">
+class App extends Component {
+  render() {
+        const todoTitles = todosData.map(todoItem => 
+           <ToDoItem key={todoItem.id} task={todoItem.text} />
+        )
+        return (
+          <div className="todo-list">
       {todoTitles}
 
     </div>
   );
+}
 }
 
 export default App;
