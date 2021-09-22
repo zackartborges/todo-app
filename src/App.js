c6baa02e26fc19b0fc65e074397c8a3906734cb9
Zack Borges <zackartborges@gmail.com>
2021-09-22 15:06:22 -0400

    adds add to do item component and styles it at bottom of list

--- src/App.js
+++ src/App.js
@@ -2,39 +2,41 @@ import './App.css';
 import { Component } from 'react';
 import ToDoItem from './components/ToDoItem'
 import todosData from './todosData';
+import AddToDoItem from './components/AddToDoItem';
 
 class App extends Component {
   constructor() {
     super() 
     this.state = {
       todos: todosData
+     }
+      this.handleChange = this.handleChange.bind(this)
     }
-    this.handleChange = this.handleChange.bind(this)
-  }
-  handleChange(id) {
-    this.setState(prevState => {
-      const updatedTodos = prevState.todos.map(todo => {
-        if (todo.id === id) {
-          return {
-            ...todo, completed: !todo.completed
+    handleChange(id) {
+      this.setState(prevState => {
+        const updatedTodos = prevState.todos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo, completed: !todo.completed
+            }
           }
+          return todo
+        })
+        return {
+          todos: updatedTodos
         }
-        return todo
       })
-      return {
-        todos: updatedTodos
-      }
-    })
-    console.log("Completed", id)
-  }
+      console.log("Completed", id)
+    }
   render() {
         const todoItems = this.state.todos.map(item => 
            <ToDoItem key={item.id} task={item.text} item={item} handleChange={this.handleChange}/>
         )
         return (
           <div className="todo-list">
-      {todoItems}
-    </div>
+            {todoItems}
+            <AddToDoItem />
+          </div>
   );
 }
 }
---
+++ src/components/AddToDoItem.js
@@ -0,0 +1,9 @@
+function AddToDoItem(props) {
+  
+  return (
+    <div className="add-todo-item">
+        <p>Add ToDo Item</p>
+    </div>
+  )
+}
+export default AddToDoItem;
