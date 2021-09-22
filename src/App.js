c0b0024ca9f3d99d1c6e753e1f8c03acd7f57e8c
Zack Borges <zackartborges@gmail.com>
2021-09-21 21:44:35 -0400

    passes down function to child component, calls id of onChange event

--- src/App.js
+++ src/App.js
@@ -1,51 +1,26 @@
 import './App.css';
 import { Component } from 'react';
 import ToDoItem from './components/ToDoItem'
-// import todosData from './todosData';
+import todosData from './todosData';
 
 class App extends Component {
   constructor() {
     super() 
     this.state = {
-      todosData:
-        [
-          {
-              id: 1,
-              text: "Take out the trash",
-              completed: true
-          },
-          {
-              id: 2,
-              text: "Grocery shopping",
-              completed: false
-          },
-          {
-              id: 3,
-              text: "Clean gecko tank",
-              completed: false
-          },
-          {
-              id: 4,
-              text: "Mow lawn",
-              completed: true
-          },
-          {
-              id: 5,
-              text: "Catch up on Arrested Development",
-              completed: false
-          }
-        ]
-      
+      todosData: todosData
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(id) {
+    console.log("Changed", id)
   }
   render() {
         const todoTitles = this.state.todosData.map(todoItem => 
-           <ToDoItem key={todoItem.id} task={todoItem.text} item={todoItem}/>
+           <ToDoItem key={todoItem.id} task={todoItem.text} item={todoItem} handleChange={this.handleChange}/>
         )
         return (
           <div className="todo-list">
       {todoTitles}
-
     </div>
   );
 }
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,7 +1,10 @@
 function ToDoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.item.completed} />
+      <input type="checkbox" 
+      checked={props.item.completed}
+      onChange={() => props.handleChange(props.item.id)}
+      />
         <p>{props.task}</p>
     </div>
   )
