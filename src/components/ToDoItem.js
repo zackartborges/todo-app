d4a96e571d196c63ea00d1c7292d98d30d4304bd
Zack Borges <zackartborges@gmail.com>
2021-09-22 14:50:36 -0400

    adds completed style to todoItem

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,11 +1,16 @@
 function ToDoItem(props) {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#cdcdcd",
+    textDecoration: "line-through"
+  }
   return (
     <div className="todo-item">
       <input type="checkbox" 
       checked={props.item.completed}
       onChange={() => props.handleChange(props.item.id)}
       />
-        <p>{props.task}</p>
+        <p style={props.item.completed ? completedStyle: null}>{props.task}</p>
     </div>
   )
 }
