c00b9b8a333144ecfe4294ce8a20d6d60e2ba280
Zack Borges <zackartborges@gmail.com>
2021-09-29 20:40:49 -0400

    adds new input to allow users to add tasks. not finished with submitting to todo arrray yet

--- src/App.js
+++ src/App.js
@@ -1,53 +1,64 @@
-import './App.css';
-import { Component } from 'react';
-import ToDoItem from './components/ToDoItem'
-import todosData from './todosData';
+import "./App.css";
+import { Component } from "react";
+import ToDoItem from "./components/ToDoItem";
+import todosData from "./todosData";
 // import AddToDoItem from './components/AddToDoItem';
 
 class App extends Component {
   constructor() {
-    super() 
+    super();
+    console.log(todosData);
     this.state = {
       todos: todosData,
-      newTask: ""
-     }
-      this.handleChange = this.handleChange.bind(this)
-      // this.handleInput = this.handleInput.bind(this)
-    }
-    handleChange(id) {
-      this.setState(prevState => {
-        const updatedTodos = prevState.todos.map(todo => {
-          if (todo.id === id) {
-            return {
-              ...todo, completed: !todo.completed
-            }
-          }
-          return todo
-        })
-        return {
-          todos: updatedTodos
-        }
-      })
-      console.log("Completed", id)
-    }
-    // handleInput(event) {
-    //   this.setState() {
-    //     newTask: event.target.value
-    //     console.log(newTask)
-    //   }
+      // newTask: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
 
-    // }
+  }
+  componentDidMount(){
+    this.setState({
+        todos: [...this.state.todos, {
+          id: undefined,
+          text: "",
+          completed: false
+      }]
+    })
+  }
+  handleChange(id) {
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos,
+      };
+    });
+    console.log("Completed", id);
+  }
+  
   render() {
-        const todoItems = this.state.todos.map(item => 
-           <ToDoItem key={item.id} task={item.text} item={item} handleChange={this.handleChange}/>
-        )
-        return (
-          <div className="todo-list">
-            {todoItems}
-            {/* <AddToDoItem handleChange={this.handleInput}/> */}
-          </div>
-  );
-}
+    const todoItems = this.state.todos.map((item) => (
+      <ToDoItem 
+        key={item.id}
+        task={item.text}
+        item={item}
+        handleChange={this.handleChange}
+       
+      />
+    ));
+    return (
+      <div className="todo-list">
+        {todoItems}
+        {/* <AddToDoItem handleChange={this.handleInput}/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,17 +1,22 @@
+import React, { useState } from "react";
+
 function ToDoItem(props) {
-  const completedStyle = {
-    fontStyle: "italic",
-    color: "#cdcdcd",
-    textDecoration: "line-through"
-  }
+  // const completedStyle = {
+  //   fontStyle: "italic",
+  //   color: "#cdcdcd",
+  //   textDecoration: "line-through",
+  // };
+  const [name, setName] = useState(props.task);
+
   return (
     <div className="todo-item">
-      <input type="checkbox" 
-      checked={props.item.completed}
-      onChange={() => props.handleChange(props.item.id)}
-      />
-        <p style={props.item.completed ? completedStyle: null}>{props.task}</p>
+      <input type="checkbox" checked={props.item.completed} onChange={() => props.handleChange(props.item.id)} />
+      {props.item.id ? (
+        <p>{props.task}</p>
+      ) : (
+        <input value={name} name="todo" onChange={(event) => setName(event.target.value)} onBlur={console.log("Worked")} />
+      )}
     </div>
-  )
+  );
 }
 export default ToDoItem;
