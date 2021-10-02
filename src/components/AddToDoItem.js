014caffb87e467e87d642a8167801241b02f9f09
Zack Borges <zackartborges@gmail.com>
2021-10-02 17:27:58 -0400

    removes border of input box

--- src/components/AddToDoItem.js
+++ src/components/AddToDoItem.js
@@ -4,9 +4,16 @@ function AddToDoItem(props) {
   const [name, setName] = useState(props.task);
   return (
     <div className="add-todo-item">
-      <div>
-        <input type="text" value={name} name="todo" onChange={(event) => setName(event.target.value)} />
-        <button onClick={() => props.addTodo(name)}>Add Todo</button>
+      <div style={{ margin: "0 0px 0 0" }}>
+        <input
+          style={{ margin: "0 15px 0 0" }}
+          type="text"
+          value={name}
+          name="todo"
+          placeholder="Add Todo"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <button onClick={() => props.addTodo(name)}>Add</button>
       </div>
     </div>
   );
